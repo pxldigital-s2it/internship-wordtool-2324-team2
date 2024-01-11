580855b06876e11c0ca254bf860af7eb6b7ab6fe
Bartvov <bart.vanoverdijn@student.pxl.be>
2024-01-11 15:27:10 +0100

    Added contextmenu in Section.tsx with trigger on section content

--- src/components/category/subcategory/Section.tsx
+++ src/components/category/subcategory/Section.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
 import { sectionClassNames } from './Section.styles';
+import {ContextMenu} from "../../index";
+import {runOpenCreateSubCategoryModal, runOpenUpdateSubCategoryModal} from "../../../middleware/modal/ModalMiddleware";
+import {categoryContextMenu} from "../../../patterns/observer";
+import {useAppDispatch} from "../../../redux/hooks";
 
 // component representing a single section
 const Section: React.FC = () => {
+    const dispatch = useAppDispatch();
+
+    const exampleContextMenuItems = [
+        {
+            handler: () => dispatch(runOpenUpdateSubCategoryModal("sub_category_1")),
+            label: categoryContextMenu.getEditLabel()
+        },
+        {
+            handler: () => console.log("Verwijderen"),
+            label: categoryContextMenu.getDeleteLabel()
+        }
+    ];
+
   return (
     <div className={sectionClassNames.section}>
-      <span className={sectionClassNames.sectionText}>Section Content</span>
+        <ContextMenu trigger={<span className={sectionClassNames.sectionText}>Section Content</span>} menuItems={exampleContextMenuItems} />
     </div>
   );
 };
