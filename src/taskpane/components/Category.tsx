e748ea9d0752ba1fa765920822e82d6290fd8820
svendp1988 <56538162+svendp1988@users.noreply.github.com>
2024-01-11 11:26:59 +0100

    tmp commit

--- src/taskpane/components/Category.tsx
+++ src/taskpane/components/Category.tsx
@@ -7,29 +7,29 @@ import { IconButton } from "@fluentui/react/lib/Button";
 // styles for category components
 const categoryClassNames = mergeStyleSets({
   arrowIcon: {
-    color: '#005a9e',
-    fontSize: '12px',
+    color: "#005a9e",
+    fontSize: "12px",
     marginRight: 10
   },
+  categoryContent: {
+    padding: "10px 20px"
+  },
   categoryHeader: {
-    alignItems: 'center',
-    backgroundColor: '#eff6fc',
-    borderBottom: '1px solid #cccccc',
-    color: '#005a9e',
-    cursor: 'pointer',
-    display: 'flex',
-    fontSize: '16px',
-    fontWeight: '600',
-    padding: '10px 20px'
+    alignItems: "center",
+    backgroundColor: "#eff6fc",
+    borderBottom: "1px solid #cccccc",
+    color: "#005a9e",
+    cursor: "pointer",
+    display: "flex",
+    fontSize: "16px",
+    fontWeight: "600",
+    padding: "10px 20px"
   },
   categoryTitle: {
-    alignItems: 'center',
-    display: 'flex',
+    alignItems: "center",
+    display: "flex",
     flexGrow: 1,
-    justifyContent: 'flex-start'
-  },
-  categoryContent: {
-    padding: '10px 20px'
+    justifyContent: "flex-start"
   }
 });
 
