01ce07df8abfd9da8670d7103aa7c6388c34bed3
Bartvov <bart.vanoverdijn@student.pxl.be>
2024-01-13 13:36:27 +0100

    Added test to test right click

--- src/components/category/subcategory/__tests__/Section.spec.tsx
+++ src/components/category/subcategory/__tests__/Section.spec.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import Section from "../Section";
-import { render } from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
+
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import {initialState as DEFAULT_MODAL_STATE} from "../../../../redux/modal/modal.slice";
 
 describe('Section Test Suite', () => {
     test('Initial render', () => {
       const { getByText } = render(<Section />);
       expect(getByText('Section Content')).toBeInTheDocument();
+    });
+
+    test('Context menu opens on right click', () => {
+        const initialState = {
+            modal: DEFAULT_MODAL_STATE
+        };
+        const mockStore = configureStore();
+        const store = mockStore(initialState);
+
+        const { getByText } = render(
+            <Provider store={store}>
+                <Section />
+            </Provider>);
+
+        const sectionContent = getByText('Section Content');
+
+        fireEvent.contextMenu(sectionContent);
+
+        const contextMenuWijzigen = getByText('Wijzigen');
+        expect(contextMenuWijzigen).toBeInTheDocument();
     });
 });
