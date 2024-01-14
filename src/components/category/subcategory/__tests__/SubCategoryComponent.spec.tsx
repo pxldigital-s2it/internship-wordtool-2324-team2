a719efe7391e96ca2d6df914b37a4fd351df0c91
raritytiks <raritytiks@pm.me>
2024-01-14 17:46:24 +0100

    Added tests for SubCategoryComponent

--- src/components/category/subcategory/__tests__/SubCategoryComponent.spec.tsx
+++ src/components/category/subcategory/__tests__/SubCategoryComponent.spec.tsx
@@ -1,10 +1,51 @@
 import React from "react";
-import SubCategoryComponent from "../SubCategoryComponent";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import CategoryComponent from "../../maincategory/CategoryComponent";
+import Category from "../../../../types/Category";
+import SubCategory from "../../../../types/SubCategory";
+import { categoryClassNames } from "../../maincategory/CategoryComponent.styles";
 
-describe('SubCategoryComponent Test Suite', () => {
-    test('Initial render', () => {
-      const { getByText } = render(<SubCategoryComponent categoryId={"category_1"}/>);
-      expect(getByText('SubCategoryComponent Content')).toBeInTheDocument();
+describe('SubCategoryComponent Integration Test Suite', () => {
+  const mockSubCategories: SubCategory[] = [
+    {
+      categoryId: "cat-1",
+      description: "SubCategory 1 Description",
+      id: "subcat-1"
+    },
+    {
+      categoryId: "cat-1",
+      description: "SubCategory 2 Description",
+      id: "subcat-2"
+    }
+  ];
+
+  const mockCategory: Category = {
+    id: "cat-1",
+    subCategories: mockSubCategories,
+    title: "Test Category"
+  };
+
+  test('SubCategoryComponent renders within CategoryComponent', () => {
+    const { getByText, queryByText, container } = render(<CategoryComponent {...mockCategory} />);
+    const categoryTitleWithCount = `${mockCategory.title} (${mockSubCategories.length})`;
+
+    // check if CategoryComponent renders correctly with title and subcategory count
+    expect(getByText(categoryTitleWithCount)).toBeInTheDocument();
+
+    // initially, SubCategoryComponents should not be visible
+    mockSubCategories.forEach(subCat => {
+      expect(queryByText(subCat.description)).not.toBeInTheDocument();
     });
+
+    // click to expand the CategoryComponent and show SubCategoryComponents
+    fireEvent.click(getByText(categoryTitleWithCount));
+
+    // check if SubCategoryComponents are now visible
+    mockSubCategories.forEach(subCat => {
+      expect(getByText(subCat.description)).toBeInTheDocument();
+    });
+
+    // check if the correct number of SubCategoryComponents are rendered
+    expect(container.querySelectorAll(`.${categoryClassNames.categoryContent} > div`).length).toBe(mockSubCategories.length);
+  });
 });
