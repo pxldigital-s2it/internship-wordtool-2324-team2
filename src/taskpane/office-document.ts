8e813032dca315b73ad2500d30f4183e43028f4c
Bartvov <bart.vanoverdijn@student.pxl.be>
2024-02-01 11:52:05 +0100

    Added debugging for adding style

--- src/taskpane/office-document.ts
+++ src/taskpane/office-document.ts
@@ -11,15 +11,50 @@ const insertText = async (categoryId: string, description: string) => {
 
           range.load("text");
           range.load("isEmpty");
+          range.load("style");
 
           await context.sync();
 
           if (!(range.isEmpty)) {
+              let styles = context.document.getStyles();
               const category = await getCategory(categoryId);
 
+              styles.load("getByNameOrNullObject");
+
+              await context.sync();
+
+              let categoryStyleName = categoryId + "Style";
+              let categoryStyle = styles.getByNameOrNullObject(categoryStyleName);
+              categoryStyle.load("isNullObject");
+              categoryStyle.load("nameLocal");
+              categoryStyle.load("shading");
+              categoryStyle.load("font");
+              categoryStyle.shading.load("backgroundPatternColor");
+              categoryStyle.font.load("color")
+
+              await context.sync();
+
+              console.log("categoryStyleNull: " + categoryStyle.isNullObject);
+              console.log("categoryStyleName: " + categoryStyle.nameLocal);
+
+              if (categoryStyle.isNullObject) {
+                  console.log("went into if statement with null object style");
+                  console.log("1");
+                  categoryStyle.font.color = "green";
+                  console.log("2");
+                  categoryStyle.shading.backgroundPatternColor = category.colour.toUpperCase();
+                  console.log("3");
+                  context.document.addStyle(categoryStyleName, "Character");
+                  console.log("4");
+
+                  await context.sync();
+              }
+
+              console.log("5");
               const insertedRange = range.insertText(" (" + description + ") ", Word.InsertLocation.after);
-              range.font.color = "black";
-              range.font.highlightColor = category.colour;
+              range.style = categoryStyleName;
+              await context.sync();
+              console.log("range.style: " + range.style);
 
               insertedRange.font.color = "red";
               insertedRange.font.highlightColor = "white";
