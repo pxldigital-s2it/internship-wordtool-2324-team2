9b532b71e942e9b514c565dc8c09fcd7a3e5e8c4
Bartvov <bart.vanoverdijn@student.pxl.be>
2024-02-20 16:32:51 +0100

    Changed functionality to not insert text if no selection is made

--- src/components/freefeedbackinput/FreeFeedbackInput.tsx
+++ src/components/freefeedbackinput/FreeFeedbackInput.tsx
@@ -14,10 +14,11 @@ const FreeFeedbackInput: React.FC<FreeFeedbackInputProps> = ({ categoryId, descr
     const [text, setText] = useState<string>("");
 
     const handleTextInsertion = async () => {
-        await insertFreeFeedback(text);
-
         if (categoryId && description) {
+            await insertFreeFeedback(text);
             await insertAndHighlightText(categoryId, description)
+        } else {
+            await insertFreeFeedback(text);
         }
     };
 
