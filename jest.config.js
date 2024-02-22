9742a38ef17d5922512f07aba880ee31df0be67a
svendp1988 <56538162+svendp1988@users.noreply.github.com>
2024-02-22 13:48:01 +0100

    T-52 - Test insertAndHighlightText

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   collectCoverage: true,
   coverageReporters: ["text", "cobertura"],
+  moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    "uuid": require.resolve("uuid")
+  },
   preset: "ts-jest",
   setupFilesAfterEnv: [
     "<rootDir>/setupTests.js"
